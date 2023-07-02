be9a079d541796acbc591960c2ca4b483d642502
Sean Belingheri <sdbel@comcast.net>
2023-07-02 13:13:14 -0700

    Needed to stage some changes

--- Homework2/name.cpp
+++ Homework2/name.cpp
@@ -10,5 +10,11 @@
 using namespace std;
 int main()
 {
-  
+    string first, middle, last, whole;
+    cout<<"Enter your full name: ";
+    cin>>first>>middle>>last;
+    whole = first + middle + last;
+    cout<<"This name has "<<whole.length()<<" letters."<<endl;
+    cout<<first.substr(0, 1)<<middle.substr(0, 1)<<last.substr(0, 1)<<endl;
+    cout<<first.substr((first.length()+1)/2)<<middle.substr((middle.length()+1)/2)<<last.substr((last.length()+1)/2)<<endl;
 }
--- temp.cpp
+++ temp.cpp
@@ -6,3 +6,12 @@
 //
 
 #include <stdio.h>
+#include <iostream>
+using namespace std;
+int main()
+{
+    double temp;
+    cout<<"Enter a temperature in F: ";
+    cin>>temp;
+    cout<<"Temperature in C is "<<(temp - 32)*(5/9.0)<<endl;
+}
