7fe5733d387de7a489581bce41baec476199bbe2
Sean Belingheri <sdbel@comcast.net>
2023-07-02 13:12:12 -0700

    Learning what rebase is

---
+++ Homework2/name.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Homework2
+//
+//  Created by Sean Belingheri on 9/8/22.
+//
+
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+int main()
+{
+  
+}
---
+++ temp.cpp
@@ -0,0 +1,8 @@
+//
+//  temp.cpp
+//  Homework2
+//
+//  Created by Sean Belingheri on 9/8/22.
+//
+
+#include <stdio.h>
